c030827647b2ae934e845e7420656d2d2d9d6969
JoonasMV <JoonasMRViljanen@gmail.com>
2024-02-16 12:40:04 +0200

    Improved webSocket room handling

--- server/index.ts
+++ server/index.ts
@@ -1,40 +1,42 @@
-import { WebSocket } from "ws"
+import { WebSocket } from "ws";
 
 const PORT: number = parseInt(process.env.PORT || "3000");
 
-const wss = new WebSocket.Server({ port: PORT});
+const wss = new WebSocket.Server({ port: PORT });
 
-let rooms: { [key: string]: WebSocket[] } = {};
+interface WebSocketWithRoom extends WebSocket {
+  room: string
+}
 
+let rooms: { [key: string]: WebSocketWithRoom[] } = {};
 
-wss.on("connection", (ws: WebSocket) => {
-  console.log("New connection")
-  let room: string;
+wss.on("connection", (ws: WebSocketWithRoom) => {
+  console.log("New connection");
 
-  ws.on("message", message => {
+  ws.on("message", (message) => {
     if (message.toString() === "connections") {
-      const keys = Object.keys(rooms)
+      const keys = Object.keys(rooms);
       for (const key of keys) {
-        console.log(key)
-        console.log(rooms[key].length)
+        console.log(key);
+        console.log(rooms[key].length);
       }
-      return
+      return;
     }
 
-    room = JSON.parse(message.toString()).room
-    console.log(room)
+    const { room }: { room: string } = JSON.parse(message.toString());
+    console.log(room);
 
     if (!Object.keys(rooms).includes(room)) {
-      console.log("Creating room")
-      rooms[room] = [] 
+      console.log("Creating room");
+      rooms[room] = [];
     }
-    rooms[room].push(ws) 
-  })
+    rooms[room].push(ws);
+    ws.room = room;
+  });
 
   ws.on("close", () => {
-    rooms[room] = rooms[room].filter(so => so !== ws) 
-    console.log("Connection ended")
-  })
-})
-
-
+    const room = ws.room;
+    rooms[room] = rooms[room].filter(con => con !== ws)
+    console.log("Connection ended");
+  });
+});
