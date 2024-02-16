1a92f1dc794750dc2a5b755f32f1bd37ea3a8598
JoonasMV <JoonasMRViljanen@gmail.com>
2024-02-16 12:22:10 +0200

    first draft of websocket room managment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,59 @@
-const ws = new WebSocket("ws://localhost:3003");
-
-ws.addEventListener("open", (_) => {
-  console.log("WebSocket connection opened")
-  ws.send("client handshake");
-});
-
-ws.addEventListener("message", e => {
-  console.log(e.data)
-})
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [roomName, setRoomName] = useState("");
+  let ws: WebSocket;
+
+  const joinRoom = () => {   
+    let room = roomName
+    if (!roomName) {
+      room = createRoom()
+    }
+
+    ws = new WebSocket("ws://localhost:3000");
+
+    ws.addEventListener("open", (_) => {
+      console.log("WebSocket connection opened");
+
+      // send room number to server
+      ws.send(JSON.stringify({ room }));
+    });
+  };
+
+  const killConnection = () => {
+    // if(ws.readyState === WebSocket.CLOSED)
+
+    ws.close();
+    console.log("Connection closed")
+    setMessage("Connection ended");
+  };
+  
+  const checkConnections = () => {
+    ws.send("connections")
+  }
+
+  const createRoom = (): string => {
+    const newRoom = (Math.random() + 1).toString(36).substring(7);
+    setRoomName(newRoom);
+    return newRoom;
+  };
+
+
   return (
     <>
       <h1>Hello world</h1>
+      <div>
+        <button onClick={joinRoom}>Join room</button>
+        <button onClick={killConnection}>End connection</button>
+        <button onClick={checkConnections}>Check connections</button>
+        <div>{message}</div>
+      </div>
+      <div>
+        <button onClick={createRoom}>Create room</button>
+        <input onChange={e => setRoomName(e.target.value)}></input>
+        <div>{roomName}</div>
+      </div>
     </>
   );
 }
--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,40 @@
 import { WebSocket } from "ws"
 
-const PORT: number = parseInt(process.env.PORT || "3003");
+const PORT: number = parseInt(process.env.PORT || "3000");
 
 const wss = new WebSocket.Server({ port: PORT});
 
-wss.on("connection", (ws: WebSocket) => {
-  console.log("new connection")
+let rooms: { [key: string]: WebSocket[] } = {};
 
-  ws.on("message", data => {
-    console.log("client data: ", data.toString())
-  
-    ws.send("Server message")
+
+wss.on("connection", (ws: WebSocket) => {
+  console.log("New connection")
+  let room: string;
+
+  ws.on("message", message => {
+    if (message.toString() === "connections") {
+      const keys = Object.keys(rooms)
+      for (const key of keys) {
+        console.log(key)
+        console.log(rooms[key].length)
+      }
+      return
+    }
+
+    room = JSON.parse(message.toString()).room
+    console.log(room)
+
+    if (!Object.keys(rooms).includes(room)) {
+      console.log("Creating room")
+      rooms[room] = [] 
+    }
+    rooms[room].push(ws) 
   })
 
-  ws.on("close", (_ws: WebSocket) => {
-    console.log("client disconnected")
+  ws.on("close", () => {
+    rooms[room] = rooms[room].filter(so => so !== ws) 
+    console.log("Connection ended")
   })
 })
+
+
